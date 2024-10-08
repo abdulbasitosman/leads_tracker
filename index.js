614fc95d3579b4d9389481ef285e12f95a351ddb
Abdul Basit Osman <www.osmanabdulbasit@gmail.com>
2024-10-08 22:34:40 +0000

    v1 of leads tracker app

---
+++ index.js
@@ -0,0 +1,49 @@
+const inputLeadEl = document.getElementById("input_lead-el");
+const inputBtnEl = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+
+let myLeads = [];
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
+tabBtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
+function render(leads) {
+  let listItems = "";
+
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+    <li> 
+        <a href="${leads[i]}" target="_blank"> ${leads[i]} </a> 
+    </li>
+    `;
+  }
+
+  ulEl.innerHTML = listItems;
+}
+
+deleteBtn.addEventListener("dblclick", () => {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+inputBtnEl.addEventListener("click", () => {
+  myLeads.push(inputLeadEl.value);
+  inputLeadEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
+  render(myLeads);
+});
